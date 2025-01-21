0ce1e9b967030ffc0140ad2a32b6a1745393e8fc
nbrait <n.brait@rug.nl>
2025-01-21 10:12:32 +0100

    add data for script

--- data_for_scripts/readme.md
+++ data_for_scripts/readme.md
@@ -8,5 +8,7 @@ RVDB_all.tsv: input for selectivity_comparison script - tsv file detectEVE
 
 RVDB_all_cut.tsv: input for selectivity_comparison script - tsv file detectEVE with min EVE length being 250 (used for SUpplementary tables)
 
-Tool_comparison_metrics_new: input for Figure_3.R script
+Tool_comparison_metrics_new: input for Figure_2.R script
 - threads, times and CPU from detectEVE, DIGS, and Palatini pipelines
+
+tool_compraison: input for Figure_3.R - EVE contig positions from Flavivirus EVEs in Anopheles
--- scripts/Figure_3.R
+++ scripts/Figure_3.R
@@ -2,7 +2,7 @@ setwd("C:/Users/nadja/Documents/LaptopAsus/PhD/detectEVE_PC")
 
 #install.packages("ggplot2")
 library(ggplot2)
-
+library(readr)
 ###########
 
 # PLOT
@@ -14,16 +14,17 @@ df <- read_tsv("tool_comparison.tsv", col_names = TRUE)
 df$Tool <- factor(df$Tool, levels = c("Lequime et al. 2017", "detectEVE", "Palatini", "DIGS"))
 df$CONTIG <- factor(df$CONTIG, levels = c("APHL01005937.1", "ATLV01019207.1", "AXCK02024744.1", "AXCL01017469.1", "AXCP01005692.1", "APCM01026709.1"))
 
-ggplot(df, aes(x = CONTIG, y = LENGTH, fill = Tool)) +
-  geom_bar(stat = "identity", position = position_dodge2(preserve = "single")) +
-  labs(title = "Comparison of tools for Flavivirus EVE detection in Anopheles genomes",
-       x = "scaffold",
-       y = "aa length") +
-  theme_light() +
-  theme(axis.text.x = element_text(angle = 30, hjust = 1)) +
-  scale_fill_manual(values = c("Lequime et al. 2017" = "grey", "DIGS" = "darkblue", "Palatini" = "orange", "detectEVE" = "darkred")) +
-  scale_y_continuous(breaks = c(100, 1000, max(df$LENGTH)), labels = c(100, 1000, max(df$LENGTH)))
-
+# old plot
+# ggplot(df, aes(x = CONTIG, y = LENGTH, fill = Tool)) +
+#   geom_bar(stat = "identity", position = position_dodge2(preserve = "single")) +
+#   labs(title = "Comparison of tools for Flavivirus EVE detection in Anopheles genomes",
+#        x = "scaffold",
+#        y = "aa length") +
+#   theme_light() +
+#   theme(axis.text.x = element_text(angle = 30, hjust = 1)) +
+#   scale_fill_manual(values = c("Lequime et al. 2017" = "grey", "DIGS" = "darkblue", "Palatini" = "orange", "detectEVE" = "darkred")) +
+#   scale_y_continuous(breaks = c(100, 1000, max(df$LENGTH)), labels = c(100, 1000, max(df$LENGTH)))
+# 
 
 ##########################
 
@@ -33,7 +34,6 @@ df <- df %>%
   complete(Tool, CONTIG, fill = list(START = NA, END = NA, LENGTH = NA))
 
 ggplot(df, aes(x = Tool, ymin = START, ymax = END, fill = Tool, colour = Tool)) +
-  # Use errorbars instead of linerange
   geom_errorbar(size = 1.5, width = 0.2) +
   
   geom_text(
